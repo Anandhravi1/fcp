c57e36ee503ef0dc69df7d317c6f31dad8d29f67
Anandhan Ravi <anandhan.r@ziffity.com>
2020-01-14 12:21:40 +0530

    aboutus and promo section

--- app/design/frontend/Foxchapel/Store/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Foxchapel/Store/Magento_Theme/requirejs-config.js
@@ -3,9 +3,10 @@
  * See COPYING.txt for license details.
  */
 
-var config = {
+var config = {    
     deps: [
         'Magento_Theme/js/responsive',
-        'Magento_Theme/js/theme'
+        'Magento_Theme/js/theme',
+        'Magento_Theme/js/custom'        
     ]
 };
