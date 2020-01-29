68221e13e8180a132c756d4df3fb1646da8b56ca
kishorekumar.kesavan <kishorekumar.kesavan@ziffity.com>
2020-01-29 18:39:28 +0530

    remove 'shop by category' category from breadcrumb

---
+++ app/code/FoxChapel/Catalog/Helper/Data.php
@@ -0,0 +1,47 @@
+<?php
+namespace FoxChapel\Catalog\Helper;
+ 
+use Magento\Catalog\Helper\Data as MainHelper;
+use Magento\Store\Model\ScopeInterface;
+ 
+class Data extends MainHelper
+{
+
+    const HIDE_CATEGORY_FROM_BREADCRUMB = 'catalog/magento_catalogpermissions/hide_category_breadcrumb';
+
+    public function getBreadcrumbPath()
+    {
+        $catid = 0;
+       $catid =  $this->scopeConfig->getValue(
+            self::HIDE_CATEGORY_FROM_BREADCRUMB,
+            ScopeInterface::SCOPE_STORE
+       );
+        if (!$this->_categoryPath) {
+            $path = [];
+            $category = $this->getCategory();
+            if ($category) {
+                $pathInStore = $category->getPathInStore();
+                $pathIds = array_reverse(explode(',', $pathInStore));
+
+                $categories = $category->getParentCategories();
+
+                // add category path breadcrumb
+                foreach ($pathIds as $categoryId) {
+                    if (isset($categories[$categoryId]) && $categories[$categoryId]->getName() && ($categoryId != $catid)) {
+                        $path['category' . $categoryId] = [
+                            'label' => $categories[$categoryId]->getName(),
+                            'link' => $this->_isCategoryLink($categoryId) ? $categories[$categoryId]->getUrl() : ''
+                        ];
+                    }
+                }
+            }
+
+            if ($this->getProduct()) {
+                $path['product'] = ['label' => $this->getProduct()->getName()];
+            }
+
+            $this->_categoryPath = $path;
+        }
+        return $this->_categoryPath;
+    }
+}
